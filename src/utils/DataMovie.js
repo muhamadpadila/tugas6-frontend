dca2d3e7eb5453380911faf8a73d7bfa0fc68782
muhamadpadila <144421993+muhamadpadila@users.noreply.github.com>
2023-11-14 08:17:13 +0700

    Update DataMovie.js

--- src/utils/DataMovie.js
+++ src/utils/DataMovie.js
@@ -35,7 +35,24 @@ const DataMovie = [
         date: "1 april 2023",
         image: "https://www.themoviedb.org/t/p/w220_and_h330_face/b0Ej6fnXAP8fK75hlyi2jKqdhHz.jpg"
     },
-    
+    {
+        id: 9,
+        title: "JHON WICK2",
+        date: "12 april 2026",
+        image: "https://www.themoviedb.org/t/p/w220_and_h330_face/hXWBc0ioZP3cN4zCu6SN3YHXZVO.jpg"
+    },
+    {
+        id: 10,
+        title: "THE SWORD SMAN",
+        date: "19 februari 2019",
+        image: "https://www.themoviedb.org/t/p/w220_and_h330_face/r08U3dwiOeStXcjYmfnRyumgKyq.jpg"
+    },
+    {
+        id: 11,
+        title: "SHREK2",
+        date: "20 maret 2023",
+        image: "https://www.themoviedb.org/t/p/w220_and_h330_face/2yYP0PQjG8zVqturh1BAqu2Tixl.jpg"
+    },
 
     ]
 
